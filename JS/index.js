76c85339d156a201eb938713d6c3ea13b1d6a1b3
Lumber45 <lumber454545@gmail.com>
2023-09-26 21:06:33 -0300

    firts commit

---
+++ JS/index.js
@@ -0,0 +1,161 @@
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Clases/////////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+
+class VideoGame {
+    constructor(name, price) {
+        this.name = name,
+            this.price = price
+    }
+}
+
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Constants /////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+let buyBtns = document.querySelectorAll('.btn-buy');
+let welcomeBanner = document.querySelector('.welcome-banner');
+let shopItems = document.querySelector('.btn-shopping-items');
+let items = shopItems.innerText;
+let user = JSON.parse(localStorage.getItem("user"));
+let canvasBody = document.querySelector('.item-list');
+let deleteIcons;
+
+
+
+
+const snakeVideogame = new VideoGame("Snake", 2300);
+const superMarioVideogame = new VideoGame("Super Mario", 1200);
+let videogames = [snakeVideogame, superMarioVideogame];
+
+
+
+
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Functions /////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+function checkLoguedUser() {
+    if (localStorage.getItem("user") != null) {
+        console.log("Usuario logueado")
+    } else {
+        alert("Please Log In or Sign In to buy")
+        setTimeout(() => {
+            window.location.href = "/templates/logIn.html";
+        }, 2);
+    }
+}
+
+function drawUsernameInBanner() {
+
+
+    if (user != null) {
+        welcomeBanner.innerHTML += `<h2>${user.username}</h2>`
+    }
+
+
+}
+
+function setButtonsValue(videogames, buttons) {
+    let counter = 0;
+    buttons.forEach(button => {
+        button.value = videogames[counter].name
+        counter++
+    });
+
+}
+
+function addItemToCart(event) {
+    items++;
+    shopItems.innerText = `${items}`
+    let videogameName = event.target.value
+    for (const game of videogames) {
+        if (game.name === videogameName) {
+            user.shoppingCart.products.push(game)
+        }
+    }
+    drawCartItems(user.shoppingCart.products)
+}
+
+
+function drawCartItems(products) {
+
+    canvasBody.innerHTML = '';
+
+  
+    for (const product of products) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<i class="fa-solid fa-circle-minus delete-icon" value="${product.name}">${product.name}</i>`;
+        canvasBody.appendChild(listItem);
+    }
+
+
+    deleteIcons = document.querySelectorAll('.delete-icon');
+    deleteIcons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            deleteCartItem(e, products)
+        })
+    })
+}
+
+function deleteCartItem(event, products) {
+
+    let videogameName = event.target.innerText;
+    let videogame;
+    for (const game of videogames) {
+        if (game.name === videogameName) {
+            videogame = game;
+            break;
+        }
+    }
+
+    let index = user.shoppingCart.products.indexOf(videogame);
+    user.shoppingCart.products.splice(index, 1);
+
+    items--
+    shopItems.innerText = `${items}`
+
+    drawCartItems(products)
+
+}
+
+
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Events ////////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+window.onload = () => {
+    drawUsernameInBanner();
+    setButtonsValue(videogames, buyBtns)
+
+
+}
+
+buyBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        checkLoguedUser()
+        addItemToCart(e)
+    })
+});
+
+
+
+
---
+++ JS/logIn.js
@@ -0,0 +1,161 @@
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Clases/////////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+class VideoGame {
+
+    cosntructor(name, price) {
+        this.name = name,
+            this.price = price
+    }
+}
+
+
+class ShoppingCart {
+
+    constructor() {
+        this.products =[],
+        this.amount =0;
+    }
+
+}
+
+
+
+class User {
+
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.score = 0,
+        this.shoppingCart = new ShoppingCart();
+    }
+}
+
+let user;
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Constants /////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+let btnSignIn = document.getElementById('btn-signIn');
+let btnLogIn = document.getElementById('btn-logIn')
+let usernameLog = document.getElementById('usernameLog')
+let passwordLog = document.getElementById('passwordLog');
+let usernameSign = document.getElementById('usernameSign');
+let emailSign = document.getElementById('emailSign');
+let password1Sign = document.getElementById('password1Sign');
+let password2Sign = document.getElementById('password2Sign');
+
+
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Functions /////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+function checkRepeatPassword(password1, password2) {
+    if (password1 !== password2) {
+        return false
+    }
+    return true;
+}
+
+function createUser(data) {
+    if (!checkRepeatPassword(data.password1, data.password2)) {
+        alert("Los passwords no coinciden, por favor corregir")
+        return null;
+    } else {
+        user = new User(data.username, data.email, data.password1)
+        return user;
+    }
+}
+
+
+function saveUserinStorage(createUser) {
+    const user = createUser;
+    const stringedUser = JSON.stringify(user)
+    if (user != null) {
+        localStorage.setItem("user", stringedUser);
+        alert("Usuario guardado con exito")
+        window.location.href = "/index.html"
+    }
+}
+
+
+function logIn() {
+
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    let data = {
+        username: usernameLog.value,
+        password: passwordLog.value
+    }
+
+    if (dataUserComparision(data, user)) {
+        alert("Log In Succesfull")
+        window.location.href = "/index.html"
+    }
+
+}
+
+function dataUserComparision(data, user) {
+
+    if (user == null) {
+        alert("User not registered")
+    }
+
+    if (data.username !== user.username) {
+        alert("Usuario no coincide");
+        return false
+    } else if (data.password !== user.password) {
+        alert("Contraseña no coincide")
+        return false;
+    }
+    return true;
+}
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+///////////////// Events ////////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+//Sign in
+btnSignIn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    let data = {
+        username: usernameSign.value,
+        email: emailSign.value,
+        password1: password1Sign.value,
+        password2: password2Sign.value
+    }
+
+    saveUserinStorage(createUser(data))
+
+})
+
+//Log In
+btnLogIn.addEventListener("click", (e) => {
+    e.preventDefault()
+    logIn()
+})
+
+
+
+
+
+
